d1fde324674c6ca1e28050eb9a8f826f6f0ed51e
Raquel Costa <raqueldelobocosta@gmail.com>
2020-10-07 21:39:45 +0100

    ficheiros

---
+++ analise_dados.R
@@ -0,0 +1,6 @@
+#Importar libraries
+library(data.table)
+
+#Importar bases de dados disponíveis em <https://data.humdata.org/dataset/movement-range-maps>
+mobilidade <- fread("C:/Users/rakac/OneDrive - Universidade de Lisboa/R/Faculdade/2.COVID19 Portugal/Partilhado/Mobilidade_COVID19/dados_mobilidade/movement-range-2020-10-04.txt")
+
